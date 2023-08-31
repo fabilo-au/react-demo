80035fb98b53b632301fc7bf5e728d5aa1642c8b
Fabian Snaith <fabian.snaith@gmail.com>
2023-08-31 21:39:59 +1000

    add api key constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
-export const APP_NAME = "Marvel";
+/* eslint-disable no-undef */
+export const APP_NAME = 'Marvel';
+export const MARVEL_API_PRIVATE_KEY = process.env.REACT_APP_API_PRIVATE_KEY;
+export const MARVEL_API_PUBLIC_KEY = process.env.REACT_APP_API_PUBLIC_KEY;
+export const MARVEL_API_URL = 'http://gateway.marvel.com/v1/public';
