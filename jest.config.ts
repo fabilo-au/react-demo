cad878be57c3ef8393a2a9b302af6b102665aa7e
Fabian Snaith <fabian.snaith@gmail.com>
2023-08-31 20:48:12 +1000

    configure Typescript & Jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+const config = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+};
+
+export default config;
