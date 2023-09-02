91fa6227cbbee495a1b17d355914f71144feda42
Fabian Snaith <fabian.snaith@gmail.com>
2023-09-02 13:17:41 +1000

    added axios mock

---
+++ __mocks__/axios.ts
@@ -0,0 +1,16 @@
+const mockAxios = {
+  get: jest.fn(() => {
+    return Promise.resolve({ 
+      config: {},
+        data: {
+          '__mocked__': true,
+        },
+        headers: {},
+        status: 200,
+        statusText: "OK",
+    });
+  }),
+  post: jest.fn(),
+};
+
+export default mockAxios;
